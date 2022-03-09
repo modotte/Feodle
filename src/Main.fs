5d548f18a588897f264398949c180854b83bb584
modotte <runecroon@gmail.com>
2022-03-09 22:23:06 +0800

    Add Elmish

--- src/Main.fs
+++ src/Main.fs
@@ -1,14 +1,28 @@
 module Main
 
 open Feliz
+open Feliz.UseElmish
+open Elmish
 open Browser.Dom
 open Fable.Core.JsInterop
 
 importSideEffects "./styles/global.scss"
 
+type Message =
+    | EntryChanged of string
+    | AddedEntry
+    | TriedNext
+
 
 module View =
-    let main = Html.h1 "Hello world"
+    let main = 
+        Html.div [
+            Html.h1 "Hello world!"
+            Html.input [
+                prop.hidden true
+                prop.autoFocus true
+            ]
+        ]
 
 ReactDOM.render(
     View.main,
